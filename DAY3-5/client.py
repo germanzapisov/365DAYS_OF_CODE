a777a52b72e6ee225865ac9c4de3fd9190a956d8
Герман <german@MacBook-Air-German.local>
2026-03-12 14:29:14 +0300

    Add WebSocket ping/pong and graceful /exit handling

--- DAY3-5/client.py
+++ DAY3-5/client.py
@@ -1,24 +1,46 @@
-import websockets
 import asyncio
+import websockets
 
 
-async def listen(websocket):
+async def listen(websocket, name):
+    """
+    Checks for incoming messages,
+    if found, immediately displays them to the client
+    """
     async for msg in websocket:
-        print(msg)
+        print(f"{msg}")
 
 
 async def send(websocket):
+    """
+    asks the user for a message and sends it to another client
+    """
     loop = asyncio.get_running_loop()
     while True:
-       msg =  loop.run_in_executor(None, input)
-       await websocket.send(msg)
+        try:
+            msg = await loop.run_in_executor(None, input, 'введите текст:\n')
+            await websocket.send(msg)
+        except websockets.exceptions.ConnectionClosedOK:
+            print("connection is closed")
+            await asyncio.sleep(0.5)
+            exit()
 
 
 async def main():
-    async with websockets.connect('ws://localhost:5000') as websocket:
-        name = input("Введите имя: ")
+    """
+    The main function connects the client to the server
+    and activates other functions (listen(), send())
+    """
+    URI = "ws://localhost:5000"
+    async with (websockets.connect
+                    ( URI,
+                    ping_interval=20,
+                    ping_timeout=60)
+                as websocket):
+        name = input("enter the name: ")
         await websocket.send(f"__name__:{name}")
-        await asyncio.gather(listen(websocket), send(websocket))
+        await asyncio.gather(listen(websocket, name), send(websocket))
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- DAY3-5/server.py
+++ DAY3-5/server.py
@@ -1,30 +1,67 @@
 import websockets
 import asyncio
+from datetime import datetime as dt
+from config import logger
+
+
+def timer():
+    """
+    return: returns the time at the current moment
+    """
+    time_now = dt.now().strftime("%d.%H.%M.%S")
+    return time_now
+
 
 clients = {}
 
+
 async def handler(websocket):
+    """
+    The function adds the user to the set,
+    checks whether messages have arrived on the server,
+    and sends them to other clients if the user has disconnected.
+    It notifies them of this.
+    """
+    name = None
     try:
-       msg = await websocket.recv()
-       if msg.startswith("__name__"):
-           name = msg.split(":", 1)[1]
-           clients[websocket] = name
-       async for msg in websocket:
-            for c, c_name in clients.items():
-                if c != websocket:
-                    await c.send(f"{name}, {msg}")
-    except Exception:
-        print("гг")
-    finally:
-        del clients[websocket]
+        msg = await websocket.recv()
+        if msg.startswith("__name__:"):
+            name = msg.split(":", 1)[1]
+            clients[websocket] = name
+            logger.debug(f"{name} the user has connected")
+        async for msg in websocket:
+            if msg == "/exit":
+                logger.info("user disconnected")
+                await websocket.close()
+                break
+            else:
+                for c, c_name in clients.items():
+                    if c != websocket:
+                        try:
+                            await c.send(f"{name}: {msg}")
+                        except websockets.ConnectionClosed:
+                            clients.pop(c, None)
+    except websockets.exceptions.ConnectionClosedOK:
+        logger.info("user disconnected")
+        clients.pop(websocket, None)
 
 
 async def main():
-    host = 'localhost'
+    """
+    The main function starts the server on the local host
+    and runs the handler() function.
+    """
+    host = "localhost"
     port = 5000
-    server = await websockets.serve(handler, host, port)
-    print('сервер запущен!')
+    server = await websockets.serve(handler,
+                                    host,
+                                    port,
+                                    ping_interval=20,
+                                    ping_timeout=60)
+    print(f"{timer()} | server is running")
+    logger.debug("server is running")
     await asyncio.Future()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
